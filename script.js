0f53c4373a1fd0322a8a785a56553861e0c104bd
Charles Larroca <larroca_1000@hotmail.com>
2023-11-12 17:31:31 -0300

    .

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-const $nome = document.querySelector('#nome')
-const $altura = document.querySelector('#altura')
-const $peso = document.querySelector('#peso')
-const $resultado = document.querySelector('#resultado')
+const $name = document.querySelector("#name");
+const $height = document.querySelector("#height");
+const $weight = document.querySelector("#weight");
+const $result = document.querySelector("#result");
 
-function calcular(){
-  let imc = Number($peso.value)/(Number($altura.value) * Number($altura.value))
-  console.log(imc)
-  let classification = ''
-  if(imc > 40) classification = 'Obesidade grau III'
-  if(imc > 35) classification = 'Obesidade grau II'
-  if(imc > 30) classification = 'Obesidade grau I'
-  if(imc > 25) classification = 'Sobrepeso'
-  if(imc > 18.5) classification = 'peso normal'
-  if(imc < 18.5) classification = 'peso abaixo do normal'
+function calc() {
+  if ($name.value !== "" && $height.value !== "" && $weight.value !== "") {
+    let imc = Number($weight.value) / (Number($height.value) * Number($height.value));
+    console.log(imc);
+    let classification = "";
+    if (imc > 40) classification = "Obesidade grau III";
+    if (imc > 35) classification = "Obesidade grau II";
+    if (imc > 30) classification = "Obesidade grau I";
+    if (imc > 25) classification = "Sobrepeso";
+    if (imc > 18.5) classification = "peso normal";
+    if (imc < 18.5) classification = "peso abaixo do normal";
 
-  $resultado.innerHTML = `${$nome.value}, seu imc é ${imc}, você está com ${classification}`
+    $result.innerHTML = `${$name.value}, seu imc é ${imc} você está com ${classification}`;
+  } else {
+    $result.innerHTML = 'Preencha todos os campos.'
+  }
 }
